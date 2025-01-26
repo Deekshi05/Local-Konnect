1666d967b5d00e3a620b62cd63476d336df82034
Tharuntej <tharuntejpeddinti3@gmail.com>
2025-01-26 17:18:17 +0530

    Added Logo to Admin page
    Logo is Placed in src/assests/"Logoname".png

---
+++ src/main_page_admin.js
@@ -0,0 +1,195 @@
+import React from "react";
+import "./main_page_admin.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import Collapsible from "react-collapsible";
+
+function MainBody() {
+  // const toggleSubMenu = () => {
+  //   const submenu = document.querySelector(".submenu");
+  //   const arrow = document.querySelector(".arrow1");
+  //   submenu.classList.toggle("active");
+  //   arrow.classList.toggle("rotate");
+  // };
+
+  // const toggleSubOrder = () => {
+  //   const submenu = document.querySelector(".subOrder");
+  //   const arrow = document.querySelector(".arrow2");
+  //   submenu.classList.toggle("active");
+  //   arrow.classList.toggle("rotate");
+  // };
+
+  return (
+    <>
+      <div class="container">
+        <aside class="sidebar">
+          <nav>
+            <ul>
+              <li class="dash">Dashboard</li>
+              {/* <li>Item Category</li> */}
+              <li>
+                <Collapsible trigger="Services">
+                  {/* <p>
+                    This is the collapsible content. It can be any element or
+                    React component you like.
+                  </p> */}
+                  <br></br>
+                  <a href="Link Here"> Add Services</a>
+                  <br></br>
+                  <a href="Link Here">Manage Services</a>
+                </Collapsible>
+              </li>
+              <li>
+                <Collapsible trigger="Items">
+                  {/* <p>
+                    This is the collapsible content. It can be any element or
+                    React component you like.
+                  </p> */}
+                  <br></br>
+                  <a href="Link Here"> Add Items</a>
+                  <br></br>
+                  <a href="Link Here">Manage Items</a>
+                </Collapsible>
+              </li>
+              <li>
+                <Collapsible trigger="Contracters">
+                  {/* <p>
+                    This is the collapsible content. It can be any element or
+                    React component you like.
+                  </p> */}
+                  <br></br>
+                  <a href="Link Here"> Add Contracters</a>
+                  <br></br>
+                  <a href="Link Here">Manage Contracters</a>
+                </Collapsible>
+              </li>
+              <li>
+                <Collapsible trigger="Orders">
+                  {/* <p>
+                    This is the collapsible content. It can be any element or
+                    React component you like.
+                  </p> */}
+                  {/* <br></br>
+                  <a href="Link Here">All Orders</a> */}
+                  <br></br>
+                  <a href="Link Here">Pending Oders</a>
+                  <br></br>
+                  <a href="Link Here">Delivered Orders</a>
+                  <br></br>
+                  <a href="Link Here">Cancelled Orders</a>
+                </Collapsible>
+              </li>
+              <li>
+                <Collapsible trigger="Tenders">
+                  {/* <p>
+                    This is the collapsible content. It can be any element or
+                    React component you like.
+                  </p> */}
+                  <br></br>
+                  <a href="Link Here"> New Tenders</a>
+                  <br></br>
+                  <a href="Link Here">Pending Tenders</a>
+                  <br></br>
+                  <a href="Link Here">Ongoing Works</a>
+                  <br></br>
+                  <a href="Link Here">Completed Works</a>
+                  <br></br>
+                  <a href="Link Here">Cancelled Tenders</a>
+                </Collapsible>
+              </li>
+              <li>
+                <Collapsible trigger="Supervisors">
+                  {/* <p>
+                    This is the collapsible content. It can be any element or
+                    React component you like.
+                  </p> */}
+                  <br></br>
+                  <a href="Link Here"> Add Supervisors</a>
+                  <br></br>
+                  <a href="Link Here">Manage Supervisors</a>
+                </Collapsible>
+              </li>
+            </ul>
+          </nav>
+        </aside>
+        <main class="main-content">
+          <section class="stats-grid">
+            <div class="card purple">
+              Total Reg Users<br></br>
+              <span></span>
+            </div>
+            <div class="card red">
+              Total Products<br></br>
+              <span> </span>
+            </div>
+            <div class="card yellow">
+              Pending Tenders<br></br>
+              <span></span>
+            </div>
+            <div class="card red">
+              Total Supervisors<br></br>
+              <span> </span>
+            </div>
+            <div class="card cyan">
+              Total Services<br></br>
+              <span></span>
+            </div>
+            <div class="card orange">
+              Ongoing Works<br></br>
+              <span></span>
+            </div>
+            <div class="card orange">
+              Completed Works<br></br>
+              <span></span>
+            </div>
+            <div class="card teal">
+              New Tenders<br></br>
+              <span></span>
+            </div>
+            <div class="card cyan">
+              All Orders<br></br>
+              <span></span>
+            </div>
+            <div class="card orange">
+              New Material Orders<br></br>
+              <span></span>
+            </div>
+            <div class="card orange">
+              Completed Material Orders<br></br>
+              <span></span>
+            </div>
+            <div class="card orange">
+              Cancelled Material Orders<br></br>
+              <span></span>
+            </div>
+            <div class="card orange">
+              Material Partners<br></br>
+              <span></span>
+            </div>
+            <div class="card orange">
+              Total Confirmed Orders<br></br>
+              <span></span>
+            </div>
+            <div class="card orange">
+              Cancelled Tenders<br></br>
+              <span></span>
+            </div>
+            <div class="card yellow">
+              Total Cancelled Orders<br></br>
+              <span></span>
+            </div>
+            <div class="card teal">
+              Total Delivered Orders<br></br>
+              <span></span>
+            </div>
+            <div class="card blue">
+              Pending Orders<br></br>
+              <span> </span>
+            </div>
+          </section>
+        </main>
+      </div>
+    </>
+  );
+}
+export default MainBody;
--- src/nav_bar_admin.js
+++ src/nav_bar_admin.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./nav_bar_admin.css";
 import moment from "moment";
+import logo from "./assets/LocalKonnectLogo.png";
 function NavBarAdmin() {
   const formattedDate = moment().format("MMMM Do YYYY");
   return (
     <div class="top-nav">
-      <h2>Local Konnect</h2>
+      {/* <h2>Local Konnect</h2> */}
+      <img src={logo} alt="Logo" />
       <h1>Dashboard</h1>
       <span>{formattedDate}</span>
     </div>
